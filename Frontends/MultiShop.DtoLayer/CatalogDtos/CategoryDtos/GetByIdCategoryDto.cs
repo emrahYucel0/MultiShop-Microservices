4b041aa3f3ddc78fea927ceedae40b619ba567d1
Emral Yücel <e.eyucel@hotmail.com>
2024-08-15 15:24:02 +0300

    Add Admin Area, Admin Theme and Category Operations

---
+++ Frontends/MultiShop.DtoLayer/CatalogDtos/CategoryDtos/GetByIdCategoryDto.cs
@@ -0,0 +1,7 @@
+﻿namespace MultiShop.DtoLayer.CatalogDtos.CategoryDtos;
+
+public class GetByIdCategoryDto
+{
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; }
+}
