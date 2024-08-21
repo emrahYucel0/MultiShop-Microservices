3a991d3d77c7fb45b4f078a2610f54f82c461bf1
Emral Yücel <e.eyucel@hotmail.com>
2024-08-22 01:29:28 +0300

    Add ProductWithCategory Operations to Catalog Service

--- Services/Catalog/MultiShop.Catalog/Controllers/ProductsController.cs
+++ Services/Catalog/MultiShop.Catalog/Controllers/ProductsController.cs
@@ -60,7 +60,7 @@ namespace MultiShop.Catalog.Controllers
             return Ok(values);
         }
 
-        [HttpGet("ProductListWithCategoryByCategoryId/{id}")]
+        [HttpGet("ProductListWithCategoryByCategoryId")]
         public async Task<IActionResult> ProductListWithCategoryByCategoryId(string id)
         {
             var values = await _productService.GetProductsWithCategoryByCatetegoryIdAsync(id);
